9cfaeb07407eac1c7fbcb9259f0fe4313c494db5
Bartosz Dec <60384439+Qsionc@users.noreply.github.com>
2023-06-23 11:39:16 +0200

    Conversion exercises implementation

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let data = s.split_once(',');
+
+        if let Some(d) = data {
+            let name = d.0;
+            match name.len() {
+                0 => return Person::default(),
+                _ => {
+                    if let Ok(age) = d.1.parse::<usize>() {
+                        return Person {
+                            name: String::from(name),
+                            age,
+                        }
+                    }
+                }
+            }
+        }
+
+        Person::default()
     }
 }
 
